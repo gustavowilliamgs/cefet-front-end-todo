d4ed55af6cc914077118a3302c4bdff19b5759d6
Flavio Coutinho <fegemo@gmail.com>
2017-09-05 13:07:04 -0300

    Exercise

--- todo.js
+++ todo.js
@@ -1,7 +1,3 @@
-let novoItemNomeEl = document.querySelector('#novo-item-nome');
-let incluirNovoItemEl = document.querySelector('#incluir-novo-item');
-
-let listaTodoEl = document.querySelector('#lista-todo');
 let itensTodo = [
   {
     nome: 'Comprar leite',
@@ -16,36 +12,3 @@ let itensTodo = [
     marcado: true
   }
 ];
-
-function insereItemNaPagina(item) {
-  let itemEl = document.createElement('li');
-  itemEl.classList.add('item-todo');
-  itemEl.innerHTML = item.nome;
-
-  listaTodoEl.append(itemEl);
-}
-
-listaTodoEl.innerHTML = '';
-itensTodo.forEach(insereItemNaPagina);
-
-
-function incluiNovoItem() {
-  let novoItem = {
-    nome: novoItemNomeEl.value,
-    data: new Date(),
-    marcado: false
-  };
-  itensTodo.push(novoItem);
-
-  insereItemNaPagina(novoItem);
-
-  novoItemNomeEl.value = '';
-  novoItemNomeEl.focus();
-}
-
-incluirNovoItemEl.addEventListener('click', incluiNovoItem);
-novoItemNomeEl.addEventListener('keyup', e => {
-  if (e.key === 'Enter') {
-    incluiNovoItem()
-  }
-});
